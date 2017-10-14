d2a9c5bcb2ff48cfe155631b239e754f38e37234
DyckGerman <dyckgerman@gmail.com>
2017-10-15 00:01:25 +0300

    added texture

--- main.cpp
+++ main.cpp
@@ -70,62 +70,90 @@ int main() {
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
-    GLuint vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &shaders::vertex_shader, NULL);
-    glCompileShader(vertexShader);
+//    GLuint vertexShader;
+//    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+//    glShaderSource(vertexShader, 1, &shaders::vertex_shader, NULL);
+//    glCompileShader(vertexShader);
+//
+//    GLuint fragmentShader;
+//    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+//    glShaderSource(fragmentShader, 1, &shaders::fragment_shader, NULL);
+//    glCompileShader(fragmentShader);
+//
+//    GLuint fragmentShader_niceGray;
+//    fragmentShader_niceGray = glCreateShader(GL_FRAGMENT_SHADER);
+//    glShaderSource(fragmentShader_niceGray, 1, &shaders::fragment_shader_niceGray, NULL);
+//    glCompileShader(fragmentShader_niceGray);
 
-    GLuint fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &shaders::fragment_shader, NULL);
-    glCompileShader(fragmentShader);
 
-    GLuint fragmentShader_niceGray;
-    fragmentShader_niceGray = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader_niceGray, 1, &shaders::fragment_shader_niceGray, NULL);
-    glCompileShader(fragmentShader_niceGray);
+    GLuint vertex_for_texture;
+    vertex_for_texture = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_for_texture, 1, &shaders::vertex_for_texture, NULL);
+    glCompileShader(vertex_for_texture);
+
+    GLuint fragment_for_texture;
+    fragment_for_texture = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_for_texture, 1, &shaders::fragment_for_texture, NULL);
+    glCompileShader(fragment_for_texture);
+
 
     GLint success;
     GLchar log[512] = "ddfds";
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    glGetShaderInfoLog(vertexShader, 512, NULL, log);
+//    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+//    glGetShaderInfoLog(vertexShader, 512, NULL, log);
+//    std::cout << "Vertex shader compillation status = " << success << "_\n";
+//    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+//    glGetShaderInfoLog(fragmentShader, 512, NULL, log);
+//    std::cout << "fragment shader compillation status = " << success << "_\n";
+//    glGetShaderiv(fragmentShader_niceGray, GL_COMPILE_STATUS, &success);
+//    glGetShaderInfoLog(fragmentShader_niceGray, 512, NULL, log);
+//    std::cout << "nice gray fragment shader compillation status = " << success << "_\n";
+
+    glGetShaderiv(vertex_for_texture, GL_COMPILE_STATUS, &success);
+    glGetShaderInfoLog(vertex_for_texture, 512, NULL, log);
     std::cout << "Vertex shader compillation status = " << success << "_\n";
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    glGetShaderInfoLog(fragmentShader, 512, NULL, log);
-    std::cout << "fragment shader compillation status = " << success << "_\n";
-    glGetShaderiv(fragmentShader_niceGray, GL_COMPILE_STATUS, &success);
-    glGetShaderInfoLog(fragmentShader_niceGray, 512, NULL, log);
+    glGetShaderiv(fragment_for_texture, GL_COMPILE_STATUS, &success);
+    glGetShaderInfoLog(fragment_for_texture, 512, NULL, log);
     std::cout << "nice gray fragment shader compillation status = " << success << "_\n";
 
-    GLuint shaderProgram, shaderProgram_forGrey;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
+    GLuint shaderProgram, shaderProgram_forGrey, shaderProgram_forTexture;
+//    shaderProgram = glCreateProgram();
+//    glAttachShader(shaderProgram, vertexShader);
+//    glAttachShader(shaderProgram, fragmentShader);
+//    glLinkProgram(shaderProgram);
+//
+//    shaderProgram_forGrey= glCreateProgram();
+//    glAttachShader(shaderProgram_forGrey, vertexShader);
+//    glAttachShader(shaderProgram_forGrey, fragmentShader_niceGray);
+//    glLinkProgram(shaderProgram_forGrey);
 
-    shaderProgram_forGrey= glCreateProgram();
-    glAttachShader(shaderProgram_forGrey, vertexShader);
-    glAttachShader(shaderProgram_forGrey, fragmentShader_niceGray);
-    glLinkProgram(shaderProgram_forGrey);
+    shaderProgram_forTexture = glCreateProgram();
+    glAttachShader(shaderProgram_forTexture, vertex_for_texture);
+    glAttachShader(shaderProgram_forTexture, fragment_for_texture);
+    glLinkProgram(shaderProgram_forTexture);
+
+    int linkStatus;
+    glGetProgramiv(shaderProgram_forTexture, GL_LINK_STATUS, &linkStatus);
 
     int verticlesCount = 100;
     int indicesCount = 30;
 
-    float verticles[verticlesCount];
+//    float verticles[verticlesCount];
 
     const long twoPow32 = 2147483648;
 
-    for (int i = 0; i < verticlesCount; i++) {
-        if ((i + 1) % 3 == 0)
-            verticles[i] = 0;
-        else
-            verticles[i] = ((GLfloat)rand() - twoPow32 / 2) / (GLfloat)twoPow32;
-    }
+    float vertices[] = {
+            // positions          // colors           // texture coords
+            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+    };
 
-    int indices[indicesCount];
-    for (int i = 0; i < indicesCount; i++)
-        indices[i] = i;
-
+    unsigned int indices[] = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+    };
 
 
 
@@ -135,35 +163,40 @@ int main() {
     glGenBuffers(1, &veo);
 
     glBindVertexArray(vao);
+
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticles), verticles, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, veo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) 0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-//        // texture loading, binding and generating
-//    int tex_width, tex_height, nrChannels;
-//    unsigned char *data = stbi_load("water_texture.jpg", &width, &height, &nrChannels, 0);
-//
-//    unsigned int texture;
-//    glGenTextures(1, &texture);
-//
-//    glBindTexture(GL_TEXTURE_2D, texture);
-//
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//
-//    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-//    glGenerateMipmap(GL_TEXTURE_2D);
-//
-//    stbi_image_free(data);
+    // texture loading, binding and generating
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int tex_width, tex_height, nrChannels;
+    unsigned char *data = stbi_load("water_texture.jpg", &tex_width, &tex_height, &nrChannels, 0);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(data);
 
     // Game loop
     while (!glfwWindowShouldClose(window)) {
@@ -172,13 +205,15 @@ int main() {
 
         // Render
         // Clear the colorbuffer
-        glClearColor(0.01f, 0.0f, 0.0f, 0.3f);
+        glClearColor(0.01f, 0.2f, 0.3f, 0.3f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram_forGrey);
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUseProgram(shaderProgram_forTexture);
         glBindVertexArray(vao);
-//        glDrawArrays(GL_TRIANGLES,0,3);
-        glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, 0);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // Swap the screen buffers
         glfwSwapBuffers(window);
@@ -186,8 +221,8 @@ int main() {
 
   // Terminate GLFW, clearing any resources allocated by GLFW.
 
-  glDeleteShader(fragmentShader);
-  glDeleteShader(vertexShader);
+//  glDeleteShader(fragmentShader);
+//  glDeleteShader(vertexShader);
   glfwTerminate();
   return 0;
 }
--- shaders.h
+++ shaders.h
@@ -35,6 +35,37 @@ namespace shaders {
       "{\n"
         "    color = vec4(0.1f, 0.5f, 0.1f, 1.0f);\n"
       "}\n\0";
+
+    const GLchar * vertex_for_texture = ""
+            "#version 330 core\n"
+            "layout (location = 0) in vec3 aPos;\n"
+            "layout (location = 1) in vec3 aColor;\n"
+            "layout (location = 2) in vec2 aTexCoord;\n"
+            "\n"
+            "out vec3 ourColor;\n"
+            "out vec2 TexCoord;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "    gl_Position = vec4(aPos, 1.0);\n"
+            "    ourColor = aColor;\n"
+            "    TexCoord = aTexCoord;\n"
+            "}\n\0";
+
+    const GLchar * fragment_for_texture = "\n"
+            "#version 330 core\n"
+            "out vec4 FragColor;\n"
+            "  \n"
+            "in vec3 ourColor;\n"
+            "in vec2 TexCoord;\n"
+            "\n"
+            "uniform sampler2D ourTexture;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "    FragColor = texture(ourTexture, TexCoord);\n"
+            "}\n\0";
+
 }
 
 #endif //OGL_PLAYGROUND_SHADERS_H_H
